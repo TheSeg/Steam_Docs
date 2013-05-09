532cf9a72b4ed93611903edf941209c153c77003
Seg <seg@segonmedia.com>
2013-05-09 15:30:11 -0700

    ::GetSummaries Example 0 finished.

--- docs/ISteamHub/examples/GetSummaries_00.js
+++ docs/ISteamHub/examples/GetSummaries_00.js
@@ -46,18 +46,6 @@
                 },
             ],
         },
-        /*
-        {
-            "guid" : 103582791432166170,
-            "abbreviation" : "puzzleagent2",
-            "name" : "Puzzle Agent 2",
-            "url" : "http://steamcommunity.com/app/94590",
-            "official" : true,
-            "public" : true,
-        },
-        */
-        
-        
     ]
   }
 }
