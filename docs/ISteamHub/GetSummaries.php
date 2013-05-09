df3edcd4141646e833e15b68212d601aefaa91a7
Seg <seg@segonmedia.com>
2013-05-09 13:37:15 -0700

    Starting ::GetSummaries

--- docs/ISteamHub/GetSummaries.php
+++ docs/ISteamHub/GetSummaries.php
@@ -4,5 +4,23 @@
             </div>
             
             <h2>Method Description</h2>
-            <p>-</p>
+            <p>Provides the bulk of information about any one particular hub.</p>
+            <p>This method provides the majority of metadata about a particular group including title, icons, description, and other information. If data about a hub is not served by another method, that data lives in <code>ISteamHub::GetSummaries</code>.</p>
+            
+            <h2>Inputs</h2>
+            
+            <dl class="dl-horizontal">
+              <dt>guid<br><span class="text-info">(string)</span></dt>
+              <dd>Group IDs to query, separated by commas.</dd>
+            </dl>
+            <dl class="dl-horizontal">
+              <dt>?</dt>
+              <dd>?</dd>
+            </dl>
+            
+            <h2>Outputs</h2>
+            
+            <h3>Examples</h3>
+            
+            
           </section>
