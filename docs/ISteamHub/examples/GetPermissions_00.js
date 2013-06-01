c8de6344c777407c5303a4fc6ebce1f441380fd0
Seg <seg@segonmedia.com>
2013-06-01 00:05:36 -0700

    Removed comments from JSON example.

--- docs/ISteamHub/examples/GetPermissions_00.js
+++ docs/ISteamHub/examples/GetPermissions_00.js
@@ -4,22 +4,18 @@
         "message" : "success",
         "guid" : 4178173,
         "appid" : 0,
-        "inviteOnly" : 0, // 1 = invite only.
-        // General Permissions
+        "inviteOnly" : 0,
         "caneditprops" : 0,
         "canmakeofficer" : 0,
         "canschedule" : 0,
         "canannounce" : 0,
         "canpotw" : 0,
-        // Invites
         "caninvite" : 0,
         "cankickmembers" : 0,
-        // Chatroom
         "canchataccess" : 0,
         "canchatkick" : 0,
         "canchatban" : 0,
         "canchatinvite" : 0,
-        // Comments
         "canpostcomments" : 0,
         "candeletecomments" : 0,
     }
