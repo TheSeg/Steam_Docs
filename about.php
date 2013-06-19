b7edaeae52b7175dbfcb590c9f72fc2fce9ed7fb
Seg <seg@segonmedia.com>
2013-06-18 18:06:29 -0700

    About content changes.

--- about.php
+++ about.php
@@ -7,7 +7,7 @@ include_once("./template/html_start.php");
 
 <div class="jumbotron">
   <h1><i class="icon-info-sign"></i> About</h1>
-  <p class="lead">Because <a href="http://theseg.github.io/" target="_blank">Seg</a> wants a better Steam WebAPI.</p>
+  <p class="lead">Because <a href="http://theseg.github.io/" target="_blank">Seg</a> wants a better <a href="https://developer.valvesoftware.com/wiki/Steam_Web_API">Steam WebAPI</a>.</p>
 </div>
 
 <div class="container">
@@ -16,7 +16,7 @@ include_once("./template/html_start.php");
     
     <div class="span10 offset1">
       <h2>Why Do This?</h2>
-      <p>During my <a href="http://theseg.github.io/">career</a>, I've worked with the Steamworks and Steam WebAPIs for a number of titles for Telltale Games. Specifically with <a href="http://steamcommunity.com/stats/PuzzleAgent2/stats/">Puzzle Agent 2</a> where I incorporated the Stats part of the Steamworks API. The results included displaying how many tax payer dollars were spent in submitting puzzles.</p>
+      <p>During my <a href="http://theseg.github.io/">career</a>, I've worked with the <a href="https://partner.steamgames.com">Steamworks</a> and <a href="https://developer.valvesoftware.com/wiki/Steam_Web_API">Steam WebAPI</a> for a number of titles for Telltale Games. Specifically with <a href="http://store.steampowered.com/app/94590/">Puzzle Agent 2</a> where I <a href="http://steamcommunity.com/stats/PuzzleAgent2/stats/">incorporated the community stats system</a> of Steamworks. The results included displaying how many tax payer dollars were spent in submitting puzzles.</p>
       <p>I've always been interested in the Steamworks and WebAPI in order to do something wonderful with data. The taxpayer info was the start of sharing how people play games. It made a single player experience more social without depending on direct social connections during gameplay.</p>
       <p>Since the layoffs at Telltale in 2012, I kept as current as I can on the Steam platform. When stumbling across the <a href="http://koraktor.de/steam-condenser/">Steam Condenser</a> project, I wanted to lend a hand. The project was using a mix of the deprecated XML data and I wanted to contribute to moving completely on the WebAPI. That's when I discovered the Steam Groups (now called Community Hubs) did not have a WebAPI interface.</p>
       <p class="lead">So I decided to write a proposal.</p>
