a1a91c8b518baa1131b5c6be68fe8e0fc0fbf408
Seg <seg@segonmedia.com>
2013-06-18 17:58:40 -0700

    Footer style & content changes.

--- template/html_end.php
+++ template/html_end.php
@@ -1,8 +1,10 @@
 
       <footer class="footer">
-        <p>Proposals developed by <a href="http://segonmedia.com/">Seg</a>. The Steam, WebAPI, and all things related are owned by <a href="http://www.valvesoftware.com/" target="_blank">Valve Software</a>. Of which I (Seg) have no affliction with at all.</p>
+        Proposals developed by <a href="http://segonmedia.com/">Seg</a>. The Steam, WebAPI, and all things related are owned by <a href="http://www.valvesoftware.com/" target="_blank">Valve Software</a>.<hr>
+        Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License v2.0</a>.<br>
+        Based on <a href="http://twitter.github.com/bootstrap/">Bootstrap</a> with the <a href="http://bootswatch.com/cyborg">Cyborg</a> theme by <a href="http://thomaspark.me/">Thomas Park</a> via <a href="http://bootswatch.com/">Bootswatch</a>.<br>
+        Icons from <a href="http://fortawesome.github.com/Font-Awesome/">Font Awesome</a>. Fonts from <a href="http://www.google.com/webfonts">Google Fonts</a>.
       </footer>
-
 
 </body>
 </html>
