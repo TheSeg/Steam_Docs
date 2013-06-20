35c7b81f507d01db1d5509ba8a1032bf545b5be6
Seg <seg@segonmedia.com>
2013-06-19 23:42:38 -0700

    ISteamHub::GetPermissions: Examples updated.

--- docs/ISteamHub/examples/GetPermissions_00.js
+++ docs/ISteamHub/examples/GetPermissions_00.js
@@ -1,22 +1,24 @@
 {
-    "response" : {
-        "success" : 1,
-        "message" : "success",
-        "guid" : 4178173,
-        "appid" : 0,
-        "inviteOnly" : 0,
-        "caneditprops" : 0,
-        "canmakeofficer" : 0,
-        "canschedule" : 0,
-        "canannounce" : 0,
-        "canpotw" : 0,
-        "caninvite" : 0,
-        "cankickmembers" : 0,
-        "canchataccess" : 0,
-        "canchatkick" : 0,
-        "canchatban" : 0,
-        "canchatinvite" : 0,
-        "canpostcomments" : 0,
-        "candeletecomments" : 0,
+    "response": {
+        "success": 1,
+        "message": "success",
+        "guid": 4178173,
+        "appid": 0,
+        "permissions": {
+            "inviteonly": 0,
+            "canEditProps": 0,
+            "canMakeOfficer": 0,
+            "canSchedule": 0,
+            "canAnnounce": 0,
+            "canPoTW": 0,
+            "canInvite": 0,
+            "canKickMembers": 0,
+            "canChatAccess": 0,
+            "canChatKick": 0,
+            "canChatBan": 0,
+            "canChatInvite": 0,
+            "canPostComments": 0,
+            "canDeleteComments": 0
+        }
     }
 }
---
+++ docs/ISteamHub/examples/GetPermissions_00.php
@@ -0,0 +1,44 @@
+<?php
+include_once("../../../template/init.php");
+
+$arr = array(
+  
+  "response" => array(
+    "success" => 1,
+    "message" => "success",
+    "guid" => 4178173,
+    "appid" => 0,
+    "permissions" => array(
+      "inviteonly" => 0,
+      "canEditProps" => 0,
+      "canMakeOfficer" => 0,
+      "canSchedule" => 0,
+      "canAnnounce" => 0,
+      "canPoTW" => 0,
+      "canInvite" => 0,
+      "canKickMembers" => 0,
+      "canChatAccess" => 0,
+      "canChatKick" => 0,
+      "canChatBan" => 0,
+      "canChatInvite" => 0,
+      "canPostComments" => 0,
+      "canDeleteComments" => 0,
+    ),
+  )
+  
+);
+
+
+$all = SteamDocs::convertToAll($arr);
+$saveAll = SteamDocs::saveAll($all,"GetPermissions_00");
+
+if ( false ) {
+  header("Content-type: text/plain;");
+  echo( $all['json']);
+} else if ( false ) {
+  header("Content-type: text/plain;");
+  echo( $all['vdf']);
+}
+
+
+?>
