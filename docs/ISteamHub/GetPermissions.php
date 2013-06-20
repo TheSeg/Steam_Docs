e169d46d52aee45263b9fe318b6b4ad41522d3a7
Seg <seg@segonmedia.com>
2013-06-19 23:31:00 -0700

    ISteamHub::GetPermissions: Content formating

--- docs/ISteamHub/GetPermissions.php
+++ docs/ISteamHub/GetPermissions.php
@@ -33,110 +33,122 @@
             </dl>
             
             <dl class="dl-horizontal">
-              <dt>inviteOnly<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>If value is <span class="badge badge-inverse">1</span>, then the group is invitation only.</dd>
+              <dt>permissions<br><?php print SteamDocs::param("array"); ?></dt>
+              <dd>Main Array.</dd>
             </dl>
             
-            <?php print SteamDocs::seeRoles(); ?>
+            <div class="row-fluid">
+              <div class="span11 offset1">
+                <div class="well well-small">
+                  
+                  <dl class="dl-horizontal">
+                    <dt>inviteOnly<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>If value is <span class="badge badge-inverse">1</span>, then the group is invitation only.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canEditProps<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership status a user needs to edit Hub details.
-                <br>
-                This includes avatars, hub description, hub title, and other high level hub details.
-              </dd>
-            </dl>
+                  <?php print SteamDocs::seeRoles(); ?>
             
-            <dl class="dl-horizontal">
-              <dt>canMakeOfficer<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to make a member an officer.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canEditProps<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership status a user needs to edit Hub details.
+                      <br>
+                      This includes avatars, hub description, hub title, and other high level hub details.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canSchedule<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to make events for the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canMakeOfficer<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to make a member an officer.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canAnnounce<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to make announcements for the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canSchedule<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to make events for the hub.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canPoTW<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to make a member the "Player of the Week" for the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canAnnounce<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to make announcements for the hub.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canInvite<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to invite new members into the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canPoTW<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to make a member the "Player of the Week" for the hub.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canKickMembers<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to kick members from the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canInvite<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to invite new members into the hub.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canChatAccess<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to enter the hub's chat room.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canKickMembers<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to kick members from the hub.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canChatKick<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to kick users from the hub's chat room.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canChatAccess<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to enter the hub's chat room.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canChatBan<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to ban users from using the hub's chat room.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canChatKick<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to kick users from the hub's chat room.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canChatInvite<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to invite users to use the hub's chat room.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canChatBan<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to ban users from using the hub's chat room.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canPostComments<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to post comments in the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canChatInvite<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to invite users to use the hub's chat room.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>canDeleteComments<br><?php print SteamDocs::param("int",false); ?></dt>
-              <dd>
-                Level of membership to delete comments in the hub.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>canPostComments<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to post comments in the hub.
+                    </dd>
+                  </dl>
+            
+                  <dl class="dl-horizontal">
+                    <dt>canDeleteComments<br><?php print SteamDocs::param("int",false); ?></dt>
+                    <dd>
+                      Level of membership to delete comments in the hub.
+                    </dd>
+                  </dl>
+                  
+                </div>
+              </div>
+            </div>
             
             
+            <h2>Examples</h2>
             
-            <h3>Examples</h3>
-            
-            <h4>Community Hub: <span class="text-info"><a href="http://steamcommunity.com/groups/SegTestGroup" target="_blank">Seg Test Group</a></span></h4>
+            <h3>Community Hub: <span class="text-info"><a href="http://steamcommunity.com/groups/SegTestGroup" target="_blank">Seg Test Group</a></span></h3>
             <blockquote>GET https://api.steampowered.com/ISteamHub/GetPermissions/v0001/?guid=4178173</blockquote>
             
             <?php print SteamDocs::CreateDocExampleFormats( __FILE__, "GetPermissions_00" ); ?>
