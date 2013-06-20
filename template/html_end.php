e62adf714d473be462c02873e4366aa918c71e1d
Seg <seg@segonmedia.com>
2013-06-19 17:14:54 -0700

    Footer content updates.

--- template/html_end.php
+++ template/html_end.php
@@ -1,7 +1,9 @@
 
       <footer class="footer">
-        Proposals developed by <a href="http://segonmedia.com/">Seg</a>. The Steam, WebAPI, and all things related are owned by <a href="http://www.valvesoftware.com/" target="_blank">Valve Software</a>.<hr>
-        Code licensed under the <a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License v2.0</a>.<br>
+        Proposals developed by <a href="http://segonmedia.com/">Seg</a>.
+        <br>
+        Steam, Steam WebAPI, Steamworks, and all things related are owned by <a href="http://www.valvesoftware.com/" target="_blank">Valve Software</a>.<hr>
+        Code licensed under their respective licenses as stated in their respective source files.<br>
         Based on <a href="http://twitter.github.com/bootstrap/">Bootstrap</a> with the <a href="http://bootswatch.com/cyborg">Cyborg</a> theme by <a href="http://thomaspark.me/">Thomas Park</a> via <a href="http://bootswatch.com/">Bootswatch</a>.<br>
         Icons from <a href="http://fortawesome.github.com/Font-Awesome/">Font Awesome</a>. Fonts from <a href="http://www.google.com/webfonts">Google Fonts</a>.
       </footer>
