e09ad1f9fda30d665d86138b7a37a68c4bc9c9f6
Seg <seg@segonmedia.com>
2013-06-19 23:20:38 -0700

    ISteamHub::GetEvents: Content changes.

--- docs/ISteamHub/GetEvents.php
+++ docs/ISteamHub/GetEvents.php
@@ -8,7 +8,7 @@
             
             <h3>Event Types</h3>
             <p>Events are classified into the following types outlined below. For the sake of API consistency, these types will have a integer assigned to the type. This allows for expansion and easy casing of types.</p>
-            <p>I am also proposing retitling these types to be more professional and useful as descriptions.</p>
+            <p>I am also proposing retitling these fields to be more professional and useful as descriptions. I have also included a "Online Event" type for live steaming events like <a href="http://www.desertbus.org/" target="_blank">Desert Bus for Hope</a>.</p>
             
             <table class="table table-condensed table-bordered table-striped table-hover">
               <thead>
@@ -22,7 +22,8 @@
               <tfoot>
                 <tr>
                   <td colspan="4">
-                    <i class="icon-asterisk icon-white"></i> Denotes new string.
+                    <i class="icon-asterisk icon-white"></i> Denotes new string.<br>
+                    <span class="text-success"><i class="icon-info icon-white"></i> Denotes new field.</span>
                   </td>
                 </tr>
               </tfoot>
@@ -43,7 +44,7 @@
                   <td>2</td>
                   <td>OtherEvent</td>
                   <td>A lil somethin somethin</td>
-                  <td>A lil somethin' somethin' <i class="icon-asterisk icon-white"></i></td>
+                  <td>A lil somethin' different <i class="icon-asterisk icon-white"></i></td>
                 </tr>
                 <tr>
                   <td>3</td>
@@ -81,11 +82,11 @@
                   <td>Out of town excursion</td>
                   <td>Group Field Trip <i class="icon-asterisk icon-white"></i></td>
                 </tr>
-                <tr class="success">
+                <tr class="text-success">
                   <td>9</td>
                   <td>OnlineEvent</td>
                   <td><small>N/A</small></td>
-                  <td>Online Streaming Event <i class="icon-asterisk icon-white"></i></td>
+                  <td>Online Streaming Event <i class="icon-asterisk icon-white"></i> <i class="icon-info icon-white"></i></td>
                 </tr>
               </tbody>
             </table>
@@ -156,98 +157,102 @@
               <dd>App ID of the query returned if <span class="label label-inverse">appid</span> input was defined and successful.</dd>
             </dl>
             
-            <div class="well">
-            
             <dl class="dl-horizontal">
               <dt>events<br><?php print SteamDocs::param("array"); ?></dt>
-              <dd></dd>
+              <dd>The main Events array. Each event returned will have an entry inside this array.</dd>
             </dl>
             
-            <dl class="dl-horizontal">
-              <dt>gid<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>Unique ID number of event item.</dd>
-            </dl>
+            <div class="row-fluid">
+              <div class="span11 offset1">
+                <div class="well well-small">
             
-            <dl class="dl-horizontal">
-              <dt>title<br><?php print SteamDocs::param("string"); ?></dt>
-              <dd>Title string of event item.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>gid<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>Unique ID number of event item.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>url<br><?php print SteamDocs::param("string"); ?></dt>
-              <dd>URL of event item.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>title<br><?php print SteamDocs::param("string"); ?></dt>
+                    <dd>Title string of event item.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>eventtime<br><?php print SteamDocs::param("unix timestamp"); ?></dt>
-              <dd>Unix timestamp of when event starts. Always returns in UTC.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>url<br><?php print SteamDocs::param("string"); ?></dt>
+                    <dd>URL of event item.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>eventtype<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>The event type. Consult the table below for what the integers represent.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>eventtime<br><?php print SteamDocs::param("unix timestamp"); ?></dt>
+                    <dd>Unix timestamp of when event starts. Always returns in UTC.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>eventAppID<br><?php print SteamDocs::param("int",true); ?></dt>
-              <dd>
-                App ID number of the game attached to the event item.
-                <br>
-                Valid only when <span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span>.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>eventtype<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>The event type. Consult the table below for what the integers represent.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>eventIP<br><?php print SteamDocs::param("string",true); ?></dt>
-              <dd>
-                IP address attached to the event item.
-                <br>
-                Valid only when <span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span> and <span class="label label-inverse">eventAppID</span> is defined.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>eventAppID<br><?php print SteamDocs::param("int",true); ?></dt>
+                    <dd>
+                      App ID number of the game attached to the event item.
+                      <br>
+                      Valid only when <span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span>.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>eventPassword<br><?php print SteamDocs::param("string",true); ?></dt>
-              <dd>
-                Password used to enter the game server attached to the event item.
-                <br>
-                Valid only when <span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span> and <span class="label label-inverse">eventAppID</span> is defined.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>eventIP<br><?php print SteamDocs::param("string",true); ?></dt>
+                    <dd>
+                      IP address attached to the event item.
+                      <br>
+                      Valid only when <span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span> and <span class="label label-inverse">eventAppID</span> is defined.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>eventurl<br><?php print SteamDocs::param("string",true); ?></dt>
-              <dd>
-                Steam launch URL attached to the event item.
-                <br>
-                Valid only in the following conditions:
-                <ul>
-                  <li><span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span> and <span class="label label-inverse">eventIP</span> is defined.</li>
-                  <li><span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">9</span>.</li>
-                </ul>
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>eventPassword<br><?php print SteamDocs::param("string",true); ?></dt>
+                    <dd>
+                      Password used to enter the game server attached to the event item.
+                      <br>
+                      Valid only when <span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span> and <span class="label label-inverse">eventAppID</span> is defined.
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>author<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>Steam ID number of the creator of the specific event item.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>eventurl<br><?php print SteamDocs::param("string",true); ?></dt>
+                    <dd>
+                      Steam launch URL attached to the event item.
+                      <br>
+                      Valid only in the following conditions:
+                      <ul>
+                        <li><span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">2</span> and <span class="label label-inverse">eventIP</span> is defined.</li>
+                        <li><span class="label label-inverse">eventtype</span> is <span class="badge badge-inverse">9</span>.</li>
+                      </ul>
+                    </dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>contents<br><?php print SteamDocs::param("string"); ?></dt>
-              <dd>HTML string of event contents.<br>Returns full contents in HTML.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>author<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>Steam ID number of the creator of the specific event item.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>createdtime<br><?php print SteamDocs::param("unix timestamp"); ?></dt>
-              <dd>Unix timestamp of when event was posted. Always returns in UTC.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>contents<br><?php print SteamDocs::param("string"); ?></dt>
+                    <dd>HTML string of event contents.<br>Returns full contents in HTML.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>comments<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>Number of comments made for this particular event.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>createdtime<br><?php print SteamDocs::param("unix timestamp"); ?></dt>
+                    <dd>Unix timestamp of when event was posted. Always returns in UTC.</dd>
+                  </dl>
             
+                  <dl class="dl-horizontal">
+                    <dt>comments<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>Number of comments made for this particular event.</dd>
+                  </dl>
+            
+                </div>
+              </div>
             </div>
             
             <h3>Examples</h3>
