cf483918167304ebf4e8874bd9489503bfafef18
Seg <seg@segonmedia.com>
2013-06-20 00:34:14 -0700

    GetStatus example update.

--- docs/ISteamHub/GetStatus.php
+++ docs/ISteamHub/GetStatus.php
@@ -34,37 +34,49 @@
             </dl>
             
             <dl class="dl-horizontal">
-              <dt>memberCount<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>Total members associated with the hub.</dd>
+              <dt>status<br><?php print SteamDocs::param("array"); ?></dt>
+              <dd>Array of status information for group.</dd>
             </dl>
             
-            <dl class="dl-horizontal">
-              <dt>membersInChat<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>Total members currently present in the hub's chat room.</dd>
-            </dl>
+            <div class="row-fluid">
+              <div class="span11 offset1">
+                <div class="well well-small">
+                  <dl class="dl-horizontal">
+                    <dt>memberCount<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>Total members associated with the hub.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>membersInGame<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>Total members associated with the hub playing any game on Steam.</dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>membersInChat<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>Total members currently present in the hub's chat room.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>membersOnline<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>
-                Total members associated with the hub that are online (with Steam).
-                <br>
-                Includes number of members who are in game.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>membersInGame<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>Total members associated with the hub playing any game on Steam.</dd>
+                  </dl>
             
-            <dl class="dl-horizontal">
-              <dt>playerofweek<br><?php print SteamDocs::param("int"); ?></dt>
-              <dd>
-                The Steam ID number of the current player of the week (if defined).
-                <br>
-                Returns <span class="badge badge-inverse">0</span> if there is no player of the week defined.
-              </dd>
-            </dl>
+                  <dl class="dl-horizontal">
+                    <dt>membersOnline<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>
+                      Total members associated with the hub that are online (with Steam).
+                      <br>
+                      Includes number of members who are in game.
+                    </dd>
+                  </dl>
+            
+                  <dl class="dl-horizontal">
+                    <dt>playerofweek<br><?php print SteamDocs::param("int"); ?></dt>
+                    <dd>
+                      The Steam ID number of the current player of the week (if defined).
+                      <br>
+                      Returns <span class="badge badge-inverse">0</span> if there is no player of the week defined.
+                    </dd>
+                  </dl>
+                  
+                </div>
+              </div>
+            </div>
             
             <h3>Examples</h3>
             
--- docs/ISteamHub/examples/GetStatus_00.js
+++ docs/ISteamHub/examples/GetStatus_00.js
@@ -4,14 +4,12 @@
         "message": "success",
         "guid": 4178173,
         "appid": 0,
-        "status": [
-            {
-                "memberCount": 0,
-                "membersInChat": 0,
-                "membersInGame": 0,
-                "membersOnline": 0,
-                "playerofweek": 0
-            }
-        ]
+        "status": {
+            "memberCount": 0,
+            "membersInChat": 0,
+            "membersInGame": 0,
+            "membersOnline": 0,
+            "playerofweek": 1
+        }
     }
 }
